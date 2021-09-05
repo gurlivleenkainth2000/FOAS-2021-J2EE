25443dbd5ef1689c59e30b5d4ae8620f89c912e3
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2021-09-05 19:48:54 +0530

    configurations

--- DemoWebPage/src/main/java/com/technotech/model/EXTRAS.java
+++ DemoWebPage/src/main/java/com/technotech/model/EXTRAS.java
@@ -2,8 +2,8 @@ package com.technotech.model;
 
 public class EXTRAS {
 	
-	public static final String CONNECTION_URL = "";
-	public static final String DATABASE = "foodOrderingDatabase";
+	public static final String CONNECTION_URL = "<connection-url>";
+	public static final String DATABASE = "<database-name>";
 	
 	public static final String COLLECTION_USER = "users";
 	
