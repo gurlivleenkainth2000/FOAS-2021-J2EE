edd1466a72387cfc2cd1544a2bf8160128cb0357
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2021-09-05 18:21:37 +0530

    User Registration and Login Module Integrated using MongoDB

---
+++ DemoWebPage/src/main/java/com/technotech/model/EXTRAS.java
@@ -0,0 +1,11 @@
+package com.technotech.model;
+
+public class EXTRAS {
+	
+	public static final String CONNECTION_URL = "";
+	public static final String DATABASE = "foodOrderingDatabase";
+	
+	public static final String COLLECTION_USER = "users";
+	
+
+}
